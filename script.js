e8300b9b6a6fa6d5b58b776d45fa5f0022edcecf
Lflores2022 <launchx03687@innovaccion.mx>
2023-03-21 17:50:33 -0600

    tercera clase terminada

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ console.log(btn)
 const createTask = (e) =>{
     e.preventDefault()
     const textTask = document.querySelector("[data-form-input]")
-    console.log(textTask.value)
+    const value = textTask.value
+    const card = document.querySelector('[data-card]')
+    const task = document.createElement('li')
+    task.classList.add("card")
+    textTask.value = ""
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML=content
+  card.appendChild(task)
+    console.log(content)
 }
 
 
